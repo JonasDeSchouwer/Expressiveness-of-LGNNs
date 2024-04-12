c1afa9f6ac8743f7decbbcfd90946cbd05df479f
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2024-04-12 11:54:04 +0200

    added code to generate dataset

---
+++ code/dataset.py
@@ -0,0 +1,138 @@
+from typing import Generator, List
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric as tg
+from torch_geometric.data import DataLoader
+
+import pickle
+import networkx as nx
+from random import random
+from tqdm import tqdm
+import math
+
+from utils import sort_for_unique_edges, subgraph_extraction, to_line_graph
+
+
+class Graph:
+    """
+    targets: 0 if the edge is missing, 1 if the edge is present
+    """
+
+    edge_index: torch.Tensor
+    node_feat: torch.Tensor
+    line_edge_index: torch.Tensor
+    targets: torch.Tensor
+
+    def __init__(self, edge_index, node_feat, line_edge_index, targets):
+        self.edge_index = edge_index
+        self.node_feat = node_feat
+        self.line_edge_index = line_edge_index
+        self.targets = targets
+
+
+class Dataset():
+    train_graphs: List[Graph]
+    test_graphs: List[Graph]
+    
+    def __init__(self, path=None):
+        self.train_graphs = None
+        self.test_graphs = None
+        if path is not None:
+            self.load(path)
+    
+    def save(self, folder):
+        with open(f"{folder}/train_graphs.pckl", 'wb') as f:
+            pickle.dump(self.train_graphs, f)
+        with open(f"{folder}/test_graphs.pckl", 'wb') as f:
+            pickle.dump(self.test_graphs, f)
+
+    def load(self, folder):
+        with open(f"{folder}/train_graphs.pckl", 'rb') as f:
+            self.train_graphs = pickle.load(f)
+        with open(f"{folder}/test_graphs.pckl", 'rb') as f:
+            self.test_graphs = pickle.load(f)
+    
+    def iter_batches(self, batch_size, train=True) -> Generator[List[Graph], None, None]:
+        graphs = self.train_graphs if train else self.test_graphs
+        n_graphs = len(graphs)
+        batch_idxs = torch.randperm(n_graphs)
+        batch_idxs = torch.chunk(batch_idxs, math.ceil(n_graphs/batch_size))
+        for batch in batch_idxs:
+            yield [graphs[i] for i in batch]
+
+
+def generate_PPI_dataset(test_ratio=0.1, num_samples_per_graph=200, pos_ratio=0.5, h=2):
+    PPI = tg.datasets.PPI(root='data')
+    num_train = int((1-test_ratio) * len(PPI))
+    num_test = len(PPI) - num_train
+    print(f"dataset contains {len(PPI)} graphs: using {num_train} for training and {num_test} for testing")
+
+    print("generating training graphs...")
+    train_graphs = []
+    for i in tqdm(range(num_train)):
+        data = PPI[i]
+        train_graphs += sample_pos(data.edge_index, data.x, int(num_samples_per_graph * pos_ratio), h)
+        train_graphs += sample_neg(data.edge_index, data.x, int(num_samples_per_graph * (1-pos_ratio)), h)
+    
+    print("generating test graphs...")
+    test_graphs = []
+    for i in tqdm(range(num_train, len(PPI))):
+        data = PPI[i]
+        test_graphs += sample_pos(data.edge_index, data.x, int(num_samples_per_graph * pos_ratio), h)
+        test_graphs += sample_neg(data.edge_index, data.x, int(num_samples_per_graph * (1-pos_ratio)), h)
+    
+    dataset = Dataset()
+    dataset.train_graphs = train_graphs
+    dataset.test_graphs = test_graphs
+
+    print(f"dataset generated: {len(train_graphs)} training graphs and {len(test_graphs)} testing graphs")
+
+    return dataset
+
+
+def sample_pos(edge_index, node_feat, num_samples, h):
+    """
+    sample num_samples positive edges from the graph and return the h-hop enclosing subgraphs
+    """
+    sort_for_unique_edges(edge_index)   # to avoid duplicates
+
+    pos_edges = edge_index[:, :int(len(edge_index[0]) / 2)]
+    pos_edge_idxs = torch.randperm(pos_edges.shape[1])[:num_samples]
+    pos_edges = pos_edges[:, pos_edge_idxs]
+
+    pos_graphs = []
+    for i in tqdm(range(num_samples)):
+        subgraph = subgraph_extraction(list(pos_edges[:, i].flatten()), edge_index, node_feat, h)
+        line_subgraph_edge_index = to_line_graph(subgraph.edge_index, node_feat.shape[0])
+        pos_graphs.append(Graph(subgraph.edge_index, subgraph.x, line_subgraph_edge_index, torch.tensor([1])))
+
+    return pos_graphs
+
+
+def sample_neg(edge_index, node_feat, num_samples, h):
+    """
+    sample num_samples negative edges from the graph and return the h-hop enclosing subgraphs
+    """
+    sort_for_unique_edges(edge_index)   # to avoid duplicates
+
+    neg_edges = []  # list of negative edges, each in the format of a tensor
+    for i in range(num_samples):
+        neg_edge = torch.tensor([int(random() * node_feat.shape[0]), int(random() * node_feat.shape[0])]).view(2,1)
+        while torch.any(torch.all(edge_index == neg_edge, dim=0)):
+            neg_edge = torch.tensor([int(random() * node_feat.shape[0]), int(random() * node_feat.shape[0])])
+        neg_edges.append(neg_edge)
+
+    neg_graphs = []
+    for i in tqdm(range(num_samples)):
+        subgraph = subgraph_extraction(list(neg_edges[:, i].flatten()), edge_index, node_feat, h)
+        line_subgraph_edge_index = to_line_graph(subgraph.edge_index, node_feat.shape[0])
+        neg_graphs.append(Graph(subgraph.edge_index, subgraph.x, line_subgraph_edge_index, torch.tensor([0])))
+
+    return neg_graphs
+
+
+if __name__ == "__main__":
+    dataset = generate_PPI_dataset()
+    dataset.save('data/PPI')
+    print("dataset saved")
---
+++ code/utils.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric as tg
+from torch_geometric.data import DataLoader
+
+import networkx as nx
+from random import random
+from tqdm import tqdm
+
+
+def subgraph_extraction(link, edge_index, node_feat, h=1, node_information=None):
+    """
+    extract the h-hop enclosing subgraph around link `link'
+    https://github.com/LeiCaiwsu/LGLP
+    """
+
+    # nodes: a list of (unique) nodes in the subgraph
+    # nodes_dist: the distance of each node to the target nodes
+    # fringe: a set of nodes on the fringe of the subgraph
+    # fringe_from_0: the set of nodes that was first reached by expanding from node 0
+    # fringe_from_1: the set of nodes that was first reached by expanding from node 1
+    dist = 0
+    nodes = [link[0], link[1]]
+    fringe_from_0 = set([link[0]])  
+    fringe_from_1 = set([link[1]])  
+    nodes_dist_to_0 = [0, -1]
+    nodes_dist_to_1 = [-1, 0]
+    for dist in range(1, h+1):
+        fringe_from_0 = neighbors(fringe_from_0, edge_index)
+        fringe_from_1 = neighbors(fringe_from_1, edge_index)
+        
+        fringe_from_0 -= set(nodes)
+        fringe_from_1 -= set(nodes)
+        
+        fringe_from_both = fringe_from_0.intersection(fringe_from_1)
+        fringe_from_0 -= fringe_from_both
+        fringe_from_1 -= fringe_from_both
+
+        if len(fringe_from_0) == len(fringe_from_1) == len(fringe_from_both) == 0:
+            break
+
+        nodes += list(fringe_from_0)
+        nodes_dist_to_0 += [dist] * len(fringe_from_0)
+        nodes_dist_to_1 += [-1] * len(fringe_from_0)
+        nodes += list(fringe_from_1)
+        nodes_dist_to_0 += [-1] * len(fringe_from_1)
+        nodes_dist_to_1 += [dist] * len(fringe_from_1)
+        nodes += list(fringe_from_both)
+        nodes_dist_to_0 += [dist] * len(fringe_from_both)
+        nodes_dist_to_1 += [dist] * len(fringe_from_both)
+
+    # create and label subgraph
+    sub_edge_index, _ = tg.utils.subgraph(nodes, edge_index, relabel_nodes=True)
+    sub_node_feat = torch.cat((
+        node_feat[nodes],
+        torch.tensor(nodes_dist_to_0).view(-1, 1).float(),
+        torch.tensor(nodes_dist_to_1).view(-1, 1).float()
+    ), dim=1)
+
+    # remove 0-1 and 1-0 edge
+    edge01 = torch.all(sub_edge_index == torch.tensor([[0],[1]]), dim=0)
+    edge10 = torch.all(sub_edge_index == torch.tensor([[1],[0]]), dim=0)
+    sub_edge_index = sub_edge_index[:, ~edge01 & ~edge10]
+
+    # make sure the first m edges are each unique
+    sub_edge_index = sort_for_unique_edges(sub_edge_index)
+
+    subgraph = tg.data.Data(edge_index=sub_edge_index, x=sub_node_feat)
+    return subgraph
+
+
+def neighbors(fringe, edge_index):
+    """
+    find all 1-hop neighbors of nodes in fringe from A
+    """
+    res = set()
+    for node in fringe:
+        idxs = edge_index[0] == node    # The idxs of the edges for which the source is node
+        neighbours = set(torch.unique(edge_index[1][idxs]).tolist())
+        res = res.union(neighbours)
+    return res
+
+
+def sort_for_unique_edges(edge_index):
+    """
+    assuming that the graph has no self-loops and is undirected
+    make sure that the first m edges are each unique and the following m edges are the duplicates
+    """
+    assert edge_index.shape[0] % 2 == 0
+    assert not torch.any(edge_index[0] == edge_index[1]), "the graph has self-loops"
+    
+    first_idxs = edge_index[0] < edge_index[1]
+    edge_index_first = edge_index[:, first_idxs]
+    edge_index_second = edge_index[:, ~first_idxs]
+    edge_index = torch.cat((edge_index_first, edge_index_second), dim=1)
+
+    return edge_index
+
+
+def to_line_graph(edge_index, num_nodes):
+    """
+    create the line graph of the input graph
+    """
+    unique_edges = edge_index[:, :int(len(edge_index[0]) / 2)]
+    assert torch.all(unique_edges[0] < unique_edges[1]), "the graph edges have not been sorted"
+    
+    num_edges = unique_edges.shape[1]
+
+    # node_occurs_in[i, j] = True if node i occurs in edge j 
+    node_occurs_in = torch.zeros((num_nodes, num_edges), dtype=torch.bool)
+    node_occurs_in[unique_edges[0], torch.arange(num_edges)] = True
+    node_occurs_in[unique_edges[1], torch.arange(num_edges)] = True
+
+    # create the line graph
+    line_edges = []
+    for node in range(num_nodes):
+        edges_with_this_node = torch.where(node_occurs_in[node])[0] #[0] because torch.where returns a tuple
+        for i in range(len(edges_with_this_node)):
+            for j in range(i+1, len(edges_with_this_node)):
+                line_edges.append(torch.tensor([edges_with_this_node[i], edges_with_this_node[j]]))
+    line_edge_index = torch.stack(line_edges, dim=0).t()
+
+    # add the other direction of the edges
+    line_edge_index = torch.cat((line_edge_index, line_edge_index[[1,0],:]), dim=1)
+
+    return line_edge_index
+
+
+def to_line_graph_features(edge_index, node_feat):
+    """
+    create the node features of the line graph by concatenating the node features of the corresponding endpoints
+    """
+    unique_edges = edge_index[:, :int(len(edge_index[0]) / 2)]
+    assert torch.all(unique_edges[0] < unique_edges[1]), "the graph edges have not been sorted"
+
+    # create the line graph features
+    line_node_feat = torch.cat((node_feat[unique_edges[0]], node_feat[unique_edges[1]]), dim=1)
+
+    return line_node_feat
